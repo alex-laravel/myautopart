b5eb5d308475135ad8bffdcd00cfc1dec2a22060
a.masalov <a.masalov@program-ace.com.ua>
2021-06-14 00:11:38 +0300

    added bootstrap

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.sass('resources/scss/app.scss', 'public/css/frontend.css')
+    .js('resources/js/app.js', 'public/js/frontend.js');
