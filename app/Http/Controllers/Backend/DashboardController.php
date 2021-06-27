6958ac573527119e23e0395ffc0db13692fffc92
a.masalov <a.masalov@program-ace.com.ua>
2021-06-28 02:28:26 +0300

    added dashboard changes

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Backend;
 
 
 use App\Http\Controllers\Controller;
+use App\Models\TecDoc\Brand;
+use App\Models\TecDoc\Manufacturer;
+use App\Models\TecDoc\ModelSeries;
+use App\Models\TecDoc\Vehicle;
 use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
@@ -14,6 +18,11 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('backend.dashboard.index');
+        return view('backend.dashboard.index', [
+            'manufacturersTotal' => Manufacturer::get()->count(),
+            'modelsTotal' => ModelSeries::get()->count(),
+            'vehiclesTotal' => Vehicle::get()->count(),
+            'brandsTotal' => Brand::get()->count(),
+        ]);
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -18,80 +18,88 @@
                     <div class="card text-white bg-primary">
                         <div class="card-body pb-0">
                             <div class="btn-group float-right">
-                                <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <svg class="c-icon">
-                                        <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-settings"></use>
-                                    </svg>
+                                <button class="btn btn-transparent p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-ellipsis-v"></i>
                                 </button>
-                                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><a class="dropdown-item" href="#">Something else here</a></div>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="{{ route('backend.manufacturers.index') }}">More details</a>
+                                </div>
                             </div>
-                            <div class="text-value-lg">9.823</div>
-                            <div>Members online</div>
+                            <div class="text-value-lg">{{ $manufacturersTotal }}</div>
+                            <div>{{ trans('menus.backend.tecdoc.cars.manufacturers.title') }}</div>
                         </div>
                         <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
                             <canvas class="chart" id="card-chart1" height="70"></canvas>
                         </div>
                     </div>
                 </div>
-                <!-- /.col-->
+
                 <div class="col-sm-6 col-lg-3">
                     <div class="card text-white bg-info">
                         <div class="card-body pb-0">
-                            <button class="btn btn-transparent p-0 float-right" type="button">
-                                <svg class="c-icon">
-                                    <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-location-pin"></use>
-                                </svg>
-                            </button>
-                            <div class="text-value-lg">9.823</div>
-                            <div>Members online</div>
+                            <div class="btn-group float-right">
+                                <button class="btn btn-transparent p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-ellipsis-v"></i>
+                                </button>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="{{ route('backend.models.index') }}">More details</a>
+                                </div>
+                            </div>
+
+                            <div class="text-value-lg">{{ $modelsTotal }}</div>
+                            <div>{{ trans('menus.backend.tecdoc.cars.models.title') }}</div>
                         </div>
                         <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
                             <canvas class="chart" id="card-chart2" height="70"></canvas>
                         </div>
                     </div>
                 </div>
-                <!-- /.col-->
+
                 <div class="col-sm-6 col-lg-3">
                     <div class="card text-white bg-warning">
                         <div class="card-body pb-0">
                             <div class="btn-group float-right">
-                                <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <svg class="c-icon">
-                                        <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-settings"></use>
-                                    </svg>
+                                <button class="btn btn-transparent p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-ellipsis-v"></i>
                                 </button>
-                                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><a class="dropdown-item" href="#">Something else here</a></div>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="{{ route('backend.vehicles.index') }}">More details</a>
+                                </div>
                             </div>
-                            <div class="text-value-lg">9.823</div>
-                            <div>Members online</div>
+                            <div class="text-value-lg">{{ $vehiclesTotal }}</div>
+                            <div>{{ trans('menus.backend.tecdoc.cars.vehicles.title') }}</div>
                         </div>
                         <div class="c-chart-wrapper mt-3" style="height:70px;">
                             <canvas class="chart" id="card-chart3" height="70"></canvas>
                         </div>
                     </div>
                 </div>
-                <!-- /.col-->
+
                 <div class="col-sm-6 col-lg-3">
                     <div class="card text-white bg-danger">
                         <div class="card-body pb-0">
                             <div class="btn-group float-right">
-                                <button class="btn btn-transparent dropdown-toggle p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <svg class="c-icon">
-                                        <use xlink:href="assets/icons/coreui/free-symbol-defs.svg#cui-settings"></use>
-                                    </svg>
+                                <button class="btn btn-transparent p-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-ellipsis-v"></i>
                                 </button>
-                                <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><a class="dropdown-item" href="#">Something else here</a></div>
+
+                                <div class="dropdown-menu dropdown-menu-right">
+                                    <a class="dropdown-item" href="{{ route('backend.brands.index') }}">More details</a>
+                                </div>
                             </div>
-                            <div class="text-value-lg">9.823</div>
-                            <div>Members online</div>
+                            <div class="text-value-lg">{{ $brandsTotal }}</div>
+                            <div>{{ trans('menus.backend.tecdoc.articles.brands.title') }}</div>
                         </div>
                         <div class="c-chart-wrapper mt-3 mx-3" style="height:70px;">
                             <canvas class="chart" id="card-chart4" height="70"></canvas>
                         </div>
                     </div>
                 </div>
-                <!-- /.col-->
             </div>
+
             <!-- /.row-->
 {{--            <div class="card">--}}
 {{--                <div class="card-body">--}}
@@ -636,5 +644,4 @@
             <!-- /.row-->
         </div>
     </div>
-
 @endsection
