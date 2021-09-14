1b827f3b9c3a1ebcc476319e13732b541c83a516
a.masalov <a.masalov@program-ace.com.ua>
2021-09-14 03:37:43 +0300

    added changes for direct article details synchronization

--- app/Http/Controllers/Backend/TecDoc/DirectArticleDetails/DirectArticleDetailsController.php
+++ app/Http/Controllers/Backend/TecDoc/DirectArticleDetails/DirectArticleDetailsController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers\Backend\TecDoc\DirectArticleDetails;
 
 use App\Http\Controllers\Controller;
-use App\Models\TecDoc\DirectArticle\DirectArticle;
 use App\Models\TecDoc\DirectArticleDetails\DirectArticleDetails;
 use App\Models\TecDoc\DirectArticleDocument\DirectArticleDocument;
+use App\Models\TecDoc\TmpDirectArticleUnique;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -94,85 +94,97 @@ class DirectArticleDetailsController extends Controller
     {
         ini_set('max_execution_time', 0);
 
-        DirectArticleDetails::truncate();
+//        DirectArticleDetails::truncate();
 
-        DirectArticle::orderBy('id')->chunk(25, function ($directArticles) {
+        TmpDirectArticleUnique::chunk(500, function ($directArticles) {
             $directArticleIds = [];
 
             foreach ($directArticles as $directArticle) {
-                if (!DirectArticleDetails::where('articleId', $directArticle->articleId)->exists()) {
-                    $directArticleIds[] = $directArticle->articleId;
-                }
+                $directArticleIds[] = $directArticle->articleId;
             }
 
-            if (!empty($directArticleIds)) {
+            if (empty($directArticleIds)) {
+                return false;
+            }
+
+            foreach (array_chunk($directArticleIds, 25) as $directArticleIdsChunk) {
                 Artisan::call('tecdoc:direct-articles-details', [
-                    'articleIds' => $directArticleIds,
+                    'articleIds' => $directArticleIdsChunk,
                 ]);
 
                 $output = Artisan::output();
                 $output = json_decode($output, true);
 
-                if ($this->hasSuccessResponse($output)) {
-                    $output = $this->getResponseDataAsArray($output);
-
-                    if (!empty($output)) {
-                        $details = [];
-
-                        foreach ($output as $articleDetails) {
-                            $detailsItem['articleId'] = $articleDetails['directArticle']['articleId'];
-                            $detailsItem['articleName'] = $articleDetails['directArticle']['articleName'];
-                            $detailsItem['articleNo'] = $articleDetails['directArticle']['articleNo'];
-                            $detailsItem['articleState'] = $articleDetails['directArticle']['articleState'];
-                            $detailsItem['articleStateName'] = $articleDetails['directArticle']['articleStateName'];
-                            $detailsItem['brandName'] = $articleDetails['directArticle']['brandName'];
-                            $detailsItem['brandNo'] = $articleDetails['directArticle']['brandNo'];
-                            $detailsItem['genericArticleId'] = $articleDetails['directArticle']['genericArticleId'];
-                            $detailsItem['flagAccessories'] = $articleDetails['directArticle']['flagAccessories'];
-                            $detailsItem['flagCertificationCompulsory'] = $articleDetails['directArticle']['flagCertificationCompulsory'];
-                            $detailsItem['flagRemanufacturedPart'] = $articleDetails['directArticle']['flagRemanufacturedPart'];
-                            $detailsItem['flagSuitedforSelfService'] = $articleDetails['directArticle']['flagSuitedforSelfService'];
-                            $detailsItem['hasAppendage'] = $articleDetails['directArticle']['hasAppendage'];
-                            $detailsItem['hasAxleLink'] = $articleDetails['directArticle']['hasAxleLink'];
-                            $detailsItem['hasCsGraphics'] = $articleDetails['directArticle']['hasCsGraphics'];
-                            $detailsItem['hasDocuments'] = $articleDetails['directArticle']['hasDocuments'];
-                            $detailsItem['hasLessDiscount'] = $articleDetails['directArticle']['hasLessDiscount'];
-                            $detailsItem['hasMarkLink'] = $articleDetails['directArticle']['hasMarkLink'];
-                            $detailsItem['hasMotorLink'] = $articleDetails['directArticle']['hasMotorLink'];
-                            $detailsItem['hasOEN'] = $articleDetails['directArticle']['hasOEN'];
-                            $detailsItem['hasPartList'] = $articleDetails['directArticle']['hasPartList'];
-                            $detailsItem['hasPrices'] = $articleDetails['directArticle']['hasPrices'];
-                            $detailsItem['hasSecurityInfo'] = $articleDetails['directArticle']['hasSecurityInfo'];
-                            $detailsItem['hasUsage'] = $articleDetails['directArticle']['hasUsage'];
-                            $detailsItem['hasVehicleLink'] = $articleDetails['directArticle']['hasVehicleLink'];
-                            $detailsItem['articleAttributes'] = json_encode($articleDetails['articleAttributes']);
-                            $detailsItem['articleThumbnails'] = json_encode($articleDetails['articleThumbnails']);
-                            $detailsItem['articleDocuments'] = json_encode($articleDetails['articleDocuments']);
-                            $detailsItem['articleInfo'] = json_encode($articleDetails['articleInfo']);
-                            $detailsItem['articlePrices'] = json_encode($articleDetails['articlePrices']);
-                            $detailsItem['eanNumber'] = json_encode($articleDetails['eanNumber']);
-                            $detailsItem['immediateAttributs'] = json_encode($articleDetails['immediateAttributs']);
-                            $detailsItem['immediateInfo'] = json_encode($articleDetails['immediateInfo']);
-                            $detailsItem['mainArticle'] = json_encode($articleDetails['mainArticle']);
-//                $detailsItem['mainArticle'] = '';
-                            $detailsItem['oenNumbers'] = json_encode($articleDetails['oenNumbers']);
-//                $detailsItem['oenNumbers'] = '';
-                            $detailsItem['usageNumbers2'] = json_encode($articleDetails['usageNumbers2']);
-                            $detailsItem['replacedByNumber'] = json_encode($articleDetails['replacedByNumber']);
-                            $detailsItem['replacedNumber'] = json_encode($articleDetails['replacedNumber']);
-                            $detailsItem['created_at'] = now();
-                            $detailsItem['updated_at'] = now();
-
-                            $details[] = $detailsItem;
-                        }
-
-                        DirectArticleDetails::insert($details);
-
-                        \Log::alert('DIRECT ARTICLE DETAILS FOR IDS [' . implode(", ", $directArticleIds) . '] CREATED!');
-                    }
+                if (!$this->hasSuccessResponse($output)) {
+                    \Log::alert('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+                    \Log::alert('FAIL DIRECT ARTICLE DETAILS RESPONSE FOR IDS [' . implode(", ", $directArticleIdsChunk) . ']!');
+                    \Log::alert('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+                    \Log::alert($output);
+                    continue;
                 }
+
+                $output = $this->getResponseDataAsArray($output);
+
+                if (empty($output)) {
+                    \Log::alert('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+                    \Log::alert('EMPTY DIRECT ARTICLE DETAILS RESPONSE FOR IDS [' . implode(", ", $directArticleIdsChunk) . ']!');
+                    \Log::alert('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+                    continue;
+                }
+
+                $details = [];
+
+                foreach ($output as $articleDetails) {
+                    $detailsItem['articleId'] = $articleDetails['directArticle']['articleId'];
+                    $detailsItem['articleName'] = $articleDetails['directArticle']['articleName'];
+                    $detailsItem['articleNo'] = $articleDetails['directArticle']['articleNo'];
+                    $detailsItem['articleState'] = $articleDetails['directArticle']['articleState'];
+                    $detailsItem['articleStateName'] = $articleDetails['directArticle']['articleStateName'];
+                    $detailsItem['brandName'] = $articleDetails['directArticle']['brandName'];
+                    $detailsItem['brandNo'] = $articleDetails['directArticle']['brandNo'];
+                    $detailsItem['genericArticleId'] = $articleDetails['directArticle']['genericArticleId'];
+                    $detailsItem['flagAccessories'] = $articleDetails['directArticle']['flagAccessories'];
+                    $detailsItem['flagCertificationCompulsory'] = $articleDetails['directArticle']['flagCertificationCompulsory'];
+                    $detailsItem['flagRemanufacturedPart'] = $articleDetails['directArticle']['flagRemanufacturedPart'];
+                    $detailsItem['flagSuitedforSelfService'] = $articleDetails['directArticle']['flagSuitedforSelfService'];
+                    $detailsItem['hasAppendage'] = $articleDetails['directArticle']['hasAppendage'];
+                    $detailsItem['hasAxleLink'] = $articleDetails['directArticle']['hasAxleLink'];
+                    $detailsItem['hasCsGraphics'] = $articleDetails['directArticle']['hasCsGraphics'];
+                    $detailsItem['hasDocuments'] = $articleDetails['directArticle']['hasDocuments'];
+                    $detailsItem['hasLessDiscount'] = $articleDetails['directArticle']['hasLessDiscount'];
+                    $detailsItem['hasMarkLink'] = $articleDetails['directArticle']['hasMarkLink'];
+                    $detailsItem['hasMotorLink'] = $articleDetails['directArticle']['hasMotorLink'];
+                    $detailsItem['hasOEN'] = $articleDetails['directArticle']['hasOEN'];
+                    $detailsItem['hasPartList'] = $articleDetails['directArticle']['hasPartList'];
+                    $detailsItem['hasPrices'] = $articleDetails['directArticle']['hasPrices'];
+                    $detailsItem['hasSecurityInfo'] = $articleDetails['directArticle']['hasSecurityInfo'];
+                    $detailsItem['hasUsage'] = $articleDetails['directArticle']['hasUsage'];
+                    $detailsItem['hasVehicleLink'] = $articleDetails['directArticle']['hasVehicleLink'];
+                    $detailsItem['articleAttributes'] = json_encode($articleDetails['articleAttributes']);
+                    $detailsItem['articleThumbnails'] = json_encode($articleDetails['articleThumbnails']);
+                    $detailsItem['articleDocuments'] = json_encode($articleDetails['articleDocuments']);
+                    $detailsItem['articleInfo'] = json_encode($articleDetails['articleInfo']);
+                    $detailsItem['articlePrices'] = json_encode($articleDetails['articlePrices']);
+                    $detailsItem['eanNumber'] = json_encode($articleDetails['eanNumber']);
+                    $detailsItem['immediateAttributs'] = json_encode($articleDetails['immediateAttributs']);
+                    $detailsItem['immediateInfo'] = json_encode($articleDetails['immediateInfo']);
+                    $detailsItem['mainArticle'] = json_encode($articleDetails['mainArticle']);
+                    $detailsItem['oenNumbers'] = json_encode($articleDetails['oenNumbers']);
+                    $detailsItem['usageNumbers2'] = json_encode($articleDetails['usageNumbers2']);
+                    $detailsItem['replacedByNumber'] = json_encode($articleDetails['replacedByNumber']);
+                    $detailsItem['replacedNumber'] = json_encode($articleDetails['replacedNumber']);
+                    $detailsItem['created_at'] = now();
+                    $detailsItem['updated_at'] = now();
+
+                    $details[] = $detailsItem;
+                }
+
+                DirectArticleDetails::insert($details);
+
+                \Log::alert('DIRECT ARTICLE DETAILS FOR IDS [' . implode(", ", $directArticleIdsChunk) . '] CREATED!');
             }
         });
+
 
         return redirect()->back();
     }
